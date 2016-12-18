724a26846449ac97d844ad4ecdb83e3fc7b826ec
Tenzin Chhosphel <tenchhos@gmail.com>
2016-12-18 11:21:35 -0500

    scoreboad with animation

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,21 @@ class Tile{
   // class attributes and constructor
   constructor(id, color) {
     this.id = id;
-    this.color = 'black';
+    this.color = 'white';
     this.musicIcon = false;
     this.tapped = false;
   }
-  // class methods
-  hasMusicIcon(bool) { this.musicIcon = bool; }
-  isTapped(bool) { this.tapped = bool; }
+  // set methods
+  setMusicIcon(bool) { this.musicIcon = bool; }
+  setTapped(bool) { this.tapped = bool; }
+  setColor (color) { this.color = color; }
+
+  // get methods
+  hasMusicIcon() { return this.musicIcon; }
 }
 
 $(document).ready(function() {
-  //var core = new MotionDetector.Core();
+  // var core = new MotionDetector.Core();
 
   // create 7 piano tiles object
   let tilesArray = new Array(7);
@@ -24,15 +28,14 @@ $(document).ready(function() {
     // randomly choose 3 tiles
     let randomIndex = new Set();
     while (randomIndex.size < 3) {
-        let temp = Math.floor(Math.random()*100)%7;
+        let temp = Math.floor(Math.random()*100)%6;
         if (!randomIndex.has(temp))
           randomIndex.add(temp);
     }
     // set color to white, assign music icon for randomly selected once
-    // rest of tiles assign black color
-    $('.tiles').each((index, el) => {
+    $('.outer').each((index, el) => {
       if (randomIndex.has(index)) {
-        $('.tiles').eq(index).css({
+        $('.outer').eq(index).css({
             'background-color': 'white',
             'background-image': 'url(\'src/img/music_icon.png\')',
             'background-size': '50px 50px',
@@ -40,66 +43,45 @@ $(document).ready(function() {
             'background-position': 'center'
           });
         // set properties of Tile objects
-        tilesArray[index].color = 'white';
-        tilesArray[index].hasMusicIcon(true);
+        tilesArray[index].setMusicIcon(true);
       }
       else {
-        $('.tiles').eq(index).css('background-color', 'black');
+        $('.outer').eq(index).css('background-image', '');
+        if(index % 2 === 0) {
+          $('.outer').eq(index).css('background-color', 'white');
+        }
+        // set properties of Tile objects
+        tilesArray[index].setMusicIcon(false);
       }
     });
   }
 
   let animateTiles = () => {
-    $('.tiles').css('animation', 'tilesAnimation 15s linear infinite');
+    $('.tiles').css('animation', 'tilesAnimation 14s linear infinite');
   }
 
   let removeAnimation = () => {
     $('.tiles').css('animation', '');
   }
 
-  // call randomTiles every 15s (animation time) to random music tiles
+  // call randomTiles every 14s (animation time) to random music tiles
+  // the game will last for 140s max
   let millisecs = 0;
-  while (millisecs <= 150000) {
+  while (millisecs <= 140000) {
     window.setTimeout(randomTiles, millisecs);
     window.setTimeout(animateTiles, millisecs);
-    window.setTimeout(removeAnimation, millisecs+15000);
-    millisecs += 15000;
+    window.setTimeout(removeAnimation, millisecs+14000);
+    millisecs += 14000;
   }
 
+  let score = 0, missesLeft = 10, hits = 0;
+  $('.outer').click(function(e,i) {
+    if (tilesArray[e.target.id.charAt(4)].hasMusicIcon()) {
+      score += 5;
+      hits++;
+      $('#hits').html(`Hits: ${hits}`);
+      $('#score').html(`Score: ${score}`);
+    }
+  });
 
-  // $('#tile0').css('background-image', 'url(https://www.tattooforaweek.com/images/music_note_tattoo.jpg)');
-  // $('#tile4').css('background-image', 'url(https://www.tattooforaweek.com/images/music_note_tattoo.jpg)');
-
-
-  // let addMusicIcon = () => {
-  //   $('.tiles').eq(2).css('background-image', 'url(https://www.tattooforaweek.com/images/music_note_tattoo.jpg)');
-  //   tile2.hasMusicIcon(true);
-  //   $('.tiles').eq(10).css('background-image', 'url(https://www.tattooforaweek.com/images/music_note_tattoo.jpg)');
-  //   tile10.hasMusicIcon(true);
-  //   console.log("add musicIcon");
-  // }
-
-  // let deleteMusicIcon = () => {
-  //   $('.tiles').eq(2).css('background-image', '');
-  //   $('.tiles').eq(10).css('background-image', '');
-  //   console.log("delete musicIcon");
-  // }
-
-  // let count = 0, millisecs = 1000;
-  // while (count < 10) {
-  //   window.setTimeout(addMusicIcon, millisecs);
-  //   window.setTimeout(deleteMusicIcon, millisecs+2000);
-  //   millisecs += 4000;
-  //   count++;
-  // }
-
-  // let score = 0, missesLeft = 10, hits = 0;
-  // $('.tiles').click(function(e,i) {
-  //   if (this.style.backgroundImage !== '') {
-  //     score += 5;
-  //     hits++;
-  //     $('#innerHits').html(`Hits: ${hits}`);
-  //     $('#currentScore').html(`Score: ${score}`);
-  //   }
-  // });
 });
